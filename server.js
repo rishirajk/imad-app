b8fb7586cc8e97d8fb502fcd98322605cb5f1b00
rishirajk <30632221+rishirajk@users.noreply.github.com>
2017-08-02 21:10:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function (req,res){
     res.send('Article one responese and will be served here.');
 });
 
+app.get('/article-two', function (req,res){
+    res.send('Article two responese and will be served here.');
+});
+
+app.get('/article-three', function (req,res){
+    res.send('Article three responese and will be served here.');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
